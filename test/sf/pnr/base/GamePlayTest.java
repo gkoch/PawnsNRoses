3f992aaf8e503b2d0b1f307a42c660621cc47aa3
gkoch <gkoch@users.sourceforge.net>
2010-11-06 07:28:54 +0000

    GamePlayTest changes

--- test/sf/pnr/base/GamePlayTest.java
+++ test/sf/pnr/base/GamePlayTest.java
@@ -3,15 +3,19 @@ package sf.pnr.base;
 import sf.pnr.io.UCI;
 import sf.pnr.io.UncloseableOutputStream;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Pattern;
 
 public class GamePlayTest {
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        final UciRunner[] players = getTestPlayers();
+        final UciRunner[] players = getPlayers();
+//        final UciRunner[] players = getTestPlayers();
         FileOutputStream debugOs = null;
         final String debugFile = System.getProperty("searchTest.debugFile");
         if (debugFile != null) {
@@ -29,6 +33,24 @@ public class GamePlayTest {
         for (UciRunner player: players) {
             player.close();
         }
+    }
+
+    private static UciRunner[] getPlayers() throws IOException {
+        final String engineDir = System.getProperty("searchTest.engineDir");
+        final String patternStr = System.getProperty("searchTest.enginePattern");
+        final Pattern executablePattern = Pattern.compile(patternStr);
+        final File[] executables = new File(engineDir).listFiles(new FileFilter() {
+            @Override
+            public boolean accept(final File file) {
+                return file.isFile() && executablePattern.matcher(file.getName()).matches();
+            }
+        });
+        final UciRunner[] players = new UciRunner[executables.length];
+        for (int i = 0, executablesLength = executables.length; i < executablesLength; i++) {
+            final File executable = executables[i];
+            players[i] = new UciRunner(executable.getName(), new ExternalUciProcess(executable.getAbsolutePath()));
+        }
+        return players;
     }
 
     private static UciRunner[] getTestPlayers() throws IOException {

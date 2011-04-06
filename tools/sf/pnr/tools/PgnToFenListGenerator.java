cbc3991f9f81db31c9c6e3b101bcf720f79e8809
gkoch <gkoch@users.sourceforge.net>
2011-04-06 07:29:57 +0100

    MaxScore filter in PgnToFenListGenerator

--- tools/sf/pnr/tools/PgnToFenListGenerator.java
+++ tools/sf/pnr/tools/PgnToFenListGenerator.java
@@ -1,8 +1,11 @@
 package sf.pnr.tools;
 
 import sf.pnr.base.Board;
+import sf.pnr.base.Engine;
 import sf.pnr.base.StringUtils;
 import sf.pnr.base.Utils;
+import sf.pnr.tests.TestUtils;
+import sf.pnr.tests.UciRunner;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -21,20 +24,37 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class PgnToFenListGenerator {
-
+    private static final int MAX_SCORE_DEPTH = 10;
     private int skipMoves;
+    private int maxScore;
     private final Map<String, Integer> fenRoots = new HashMap<String, Integer>();
     private final List<String> fens = new ArrayList<String>(10000);
+    private int skippedEarly = 0;
+    private int skippedMaxScore = 0;
+    private UciRunner[] referenceEngines;
 
-    public PgnToFenListGenerator(final int skipMoves) {
+    public static void main(final String[] args) throws IOException {
+        final PgnToFenListGenerator generator = new PgnToFenListGenerator();
+        final UciRunner[] referenceEngines = TestUtils.getReferenceEngines();
+        generator.setSkipMoves(13);
+        generator.setMaxScore(800);
+        generator.setReferenceEngines(referenceEngines);
+        //generator.saveDistinct("med-games.epd");
+        generator.convert(new File(args[0]));
+        generator.saveDistinct(args[1]);
+        //generator.saveDuplicates(args[1]);
+    }
+
+    private void setReferenceEngines(final UciRunner[] referenceEngines) {
+        this.referenceEngines = referenceEngines;
+    }
+
+    public void setSkipMoves(final int skipMoves) {
         this.skipMoves = skipMoves;
     }
 
-    public static void main(final String[] args) throws IOException {
-        final PgnToFenListGenerator generator = new PgnToFenListGenerator(10);
-        generator.convert(new File(args[0]));
-        //generator.saveDistinct(args[1]);
-        generator.saveDuplicates(args[1]);
+    public void setMaxScore(final int maxScore) {
+        this.maxScore = maxScore;
     }
 
     private void saveDistinct(final String arg) throws IOException {
@@ -82,11 +102,13 @@ public class PgnToFenListGenerator {
                         final ZipEntry zipEntry = entries.nextElement();
                         System.out.println("  processing zip entry " + zipEntry.getName());
                         convert(zipFile.getInputStream(zipEntry));
-                        System.out.printf("Number of FENs: %d \r\n", fens.size());
+                        System.out.printf("FEN count: %d, skippedEarly %d, skippedMaxScore %d\r\n",
+                            fens.size(), skippedEarly, skippedMaxScore);
                     }
                 } else {
                     convert(new FileInputStream(file));
-                    System.out.printf("Number of FENs: %d \r\n", fens.size());
+                    System.out.printf("FEN count: %d, skippedEarly %d, skippedMaxScore %d\r\n",
+                        fens.size(), skippedEarly, skippedMaxScore);
                 }
 
             }
@@ -139,6 +161,13 @@ public class PgnToFenListGenerator {
             board.move(move);
             final int fullMoves = (board.getState() & Utils.FULL_MOVES) >> Utils.SHIFT_FULL_MOVES;
             if (fullMoves > skipMoves && !board.isMate()) {
+                if (maxScore > 0) {
+                    final int score = getScore(board);
+                    if (score > maxScore || score < -maxScore) {
+                        skippedMaxScore++;
+                        continue;
+                    }
+                }
                 final String fen = StringUtils.toFen(board);
                 final String fenRoot = getFenWithoutMoveCounts(fen);
                 final Integer count = fenRoots.get(fenRoot);
@@ -148,8 +177,12 @@ public class PgnToFenListGenerator {
                 } else {
                     fenRoots.put(fenRoot, count + 1);
                 }
+            } else {
+                skippedEarly++;
             }
         }
+        System.out.printf("FEN count: %d, skippedEarly %d, skippedMaxScore %d\r\n",
+            fens.size(), skippedEarly, skippedMaxScore);
     }
 
     public static String getFenWithoutMoveCounts(final String fen) {
@@ -159,5 +192,15 @@ public class PgnToFenListGenerator {
             builder.append(parts[i]).append(" ");
         }
         return builder.toString().trim();
+    }
+
+    public int getScore(final Board board) {
+        int score = 0;
+        for (UciRunner engine : referenceEngines) {
+            TestUtils.compute(engine, board, MAX_SCORE_DEPTH, 0, false);
+            score += engine.getScore();
+        }
+        return score / referenceEngines.length;
+
     }
 }

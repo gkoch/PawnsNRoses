435c72825dc89dd99729d5bb4a4c8deca8361dc0
gkoch <gkoch@users.sourceforge.net>
2011-04-21 08:05:46 +0100

    New style PerformanceRegressionTest

--- tools/sf/pnr/tests/PerformanceRegressionTest.java
+++ tools/sf/pnr/tests/PerformanceRegressionTest.java
@@ -22,17 +22,9 @@ public class PerformanceRegressionTest {
         testFiles.add("best7.epd");
         testFiles.add("wnperm.epd");
         testFiles.add("qtest_easy.epd");
-        final UciRunner pnrV0029 = new UciRunner("Pawns N' Roses v0.029b",
-            new ExternalUciProcess(engineDir + "/PawnsNRoses/v0.02x/PawnsNRoses v0.029b.bat"));
-        final Map<String, String> options = new HashMap<String, String>();
-        options.put(UCI.toUciOption(Configurable.Key.TRANSP_TABLE_SIZE), "128");
-        options.put(UCI.toUciOption(Configurable.Key.EVAL_TABLE_SIZE), "8");
-        final UciRunner pnrLatest = new UciRunner("Pawns N' Roses Latest", options, new PipedUciProcess());
-        final UciRunner rybka22 = new UciRunner("Rybka 2.2 - 2 cores",
-            new ExternalUciProcess(engineDir + "/Rybka/Rybka v2.2n2.mp.w32.exe\""));
-        final List<UciRunner> runners = Arrays.asList(rybka22, pnrLatest, pnrV0029);
+        final UciRunner[] runners = TestUtils.getEngines();
         try {
-            new EpdProcessor().process(testFiles, new MultiEngineSearchTask(runners, 6, 0, 20));
+            new EpdProcessor().process(testFiles, new MultiEngineSearchTask(Arrays.asList(runners), 6, 0, 20));
         } finally {
             for (UciRunner runner : runners) {
                 runner.close();
